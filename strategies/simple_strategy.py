4df128a0a7ad325d965b95025c8da1fd48eb6d27
JR24117 <70853077+JR24117@users.noreply.github.com>
2025-02-02 22:24:23 -0500

    Update simple_strategy.py

--- strategies/simple_strategy.py
+++ strategies/simple_strategy.py
@@ -1,16 +1,16 @@
-def moving_average_strategy(data, short_window=10, long_window=50):
+def moving_average_strategy(data, short_window=5, long_window=20):
     """
-    A simple moving average crossover strategy.
-    :param data: A DataFrame containing 'close' prices.
-    :param short_window: Window size for the short moving average.
-    :param long_window: Window size for the long moving average.
-    :return: Buy/Sell signals as a list.
+    A moving average crossover strategy with optimized parameters.
+    :param data: DataFrame containing 'close' prices.
+    :param short_window: Short moving average window (default: 5).
+    :param long_window: Long moving average window (default: 20).
+    :return: Buy/Sell signals.
     """
     data['short_ma'] = data['close'].rolling(window=short_window).mean()
     data['long_ma'] = data['close'].rolling(window=long_window).mean()
 
     data['signal'] = 0
-    data.loc[data['short_ma'] > data['long_ma'], 'signal'] = 1
-    data.loc[data['short_ma'] <= data['long_ma'], 'signal'] = -1
+    data.loc[data['short_ma'] > data['long_ma'], 'signal'] = 1  # Buy
+    data.loc[data['short_ma'] <= data['long_ma'], 'signal'] = -1  # Sell
 
     return data['signal']
